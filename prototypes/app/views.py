da0ae077afaaa307b7e5dd8a7c3899200be10118
feroz <ferozkhan27@gmail.com>
2014-07-11 00:19:35 +0800

    add prototype details method

--- prototypes/app/views.py
+++ prototypes/app/views.py
@@ -1,12 +1,17 @@
 
 import os
-
+import sys
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def prototype_listing(request):
     context = {}
-    templates = os.listdir(settings.PROTOTYPE_DIR)
-    context['templates'] = templates
+    context['prototypes'] = os.listdir(settings.PROTOTYPE_DIR)
     return render(request, 'prototype-listing.html', context)
+
+
+def prototype_details(request, slug):
+    prototype = 'prototypes/{0}'.format(slug)
+    return render(request, prototype)
--- prototypes/urls.py
+++ prototypes/urls.py
@@ -3,4 +3,5 @@ from prototypes.app import views
 
 urlpatterns = patterns('',
     url(r'$', views.prototype_listing, name='prototypes_listing'),
+    url(r'^prototypes/(?P<slug>[\w.\-]+)$', views.prototype_details, name='prototype')
 )
