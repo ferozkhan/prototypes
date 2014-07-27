9e51d9cb93f9f28cabd2bf100bc9df88b6a18708
feroz <ferozkhan27@gmail.com>
2014-07-28 02:57:28 +0800

    update urls, ini.

--- prototypes/urls.py
+++ prototypes/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from prototypes.app import views
 
+
 urlpatterns = patterns('',
-    url(r'^$', views.prototype_listing, name='prototypes_listing'),
+    url(r'^prototypes/$', views.prototype_listing, name='prototypes_listing'),
     url(r'^prototypes/(?P<slug>[\w.\-]+)$', views.prototype_details, name='prototype_details')
 )
