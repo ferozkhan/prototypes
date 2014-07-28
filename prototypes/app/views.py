2750a042f5ec8278a5503abcd7e5431f78f19991
feroz <ferozkhan27@gmail.com>
2014-07-29 00:06:03 +0800

    validation cleanup

--- prototypes/app/views.py
+++ prototypes/app/views.py
@@ -2,7 +2,6 @@
 import os
 from django import forms
 from django.conf import settings
-from django.core.validators import URLValidator
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
@@ -21,28 +20,14 @@ DEFAULT_PROTOTYPE = """
 """
 
 
-
 class PrototypeForm(forms.Form):
-    name = forms.CharField(label='name', max_length=15, min_length=3, required=True)
+    name = forms.SlugField(label='name', max_length=15, min_length=3, required=True)
     prototype = forms.CharField(label='prototype', widget=forms.Textarea(attrs={
         'cols': 100, 'rows': 50
     }), required=True)
 
     def save(self, data):
         create_prototype(data)
-
-
-def validate_form(form):
-    if form.is_valid():
-        validate = URLValidator()
-        try:
-            validate(form.cleaned_data['name'])
-        except:
-            raise forms.ValidationError('please enter a valid prototype name.')
-
-        if len(form.cleaned_data['prototype']) > MAX_PROTOTYPE_LIMIT:
-            raise forms.ValidationError('prototype size must be less than 5MB')
-
 
 
 def remove_prototype(request):
@@ -63,11 +48,11 @@ def prototype_listing(request):
     form = PrototypeForm(initial={'prototype': DEFAULT_PROTOTYPE})
     if request.method == 'POST':
         form = PrototypeForm(request.POST)
-        try:
-            validate_form(form)
-            form.save(form.cleaned_data)
-        except Exception, ex:
-            form.errors['error'] = ' ' + ex.message
+        if form.is_valid():
+            form_data = form.cleaned_data
+            if len(form_data['prototype']) > MAX_PROTOTYPE_LIMIT:
+                raise forms.ValidationError('prototype size must be less than 5MB')
+            form.save(form_data)
 
     context = {
         'prototypes': os.listdir(settings.PROTOTYPE_DIR),
