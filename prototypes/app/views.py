f95b1257d4367ba0fe1e525a527887a8721b2445
feroz <ferozkhan27@gmail.com>
2014-07-28 13:50:22 +0800

    add prototype form.

--- prototypes/app/views.py
+++ prototypes/app/views.py
@@ -1,12 +1,53 @@
 
 import os
+from django import forms
 from django.conf import settings
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+
+default_prototype = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PROTYPE TITLE</title>
+</head>
+<body>
+    PROTYPE BODY
+</body>
+</html>
+"""
+
+
+def create_prototype(data):
+    with open(os.path.join(settings.PROTOTYPE_DIR, data['name']), 'w+') as p:
+        p.write(data['prototype'])
+    return HttpResponseRedirect('/prototypes/')
+
+
+class PrototypeForm(forms.Form):
+    name = forms.CharField(label='name', max_length=15, min_length=3, required=True)
+    prototype = forms.CharField(label='prototype', widget=forms.Textarea(attrs={'cols': 100, 'rows': 50}), required=True)
+
+    def save(self, data):
+        return create_prototype(data)
 
 
 def prototype_listing(request):
     context = {}
-    context['prototypes'] = os.listdir(settings.PROTOTYPE_DIR)
+    form = PrototypeForm(initial={'prototype': default_prototype})
+    if request.method == 'POST':
+        form = PrototypeForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            form_data['name'] = form_data['name'] + '.html'
+            form.save(form_data)
+
+    context = {
+        'prototypes': os.listdir(settings.PROTOTYPE_DIR),
+        'form': form
+    }
+    print context
     return render(request, 'prototype-listing.html', context)
 
 
