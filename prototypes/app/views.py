5bdab288b16666f2319f7b9698ba865f880abf6c
feroz <ferozkhan27@gmail.com>
2014-07-28 23:00:22 +0800

    update validation.

--- prototypes/app/views.py
+++ prototypes/app/views.py
@@ -2,6 +2,7 @@
 import os
 from django import forms
 from django.conf import settings
+from django.core.validators import URLValidator
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
@@ -28,9 +29,19 @@ class PrototypeForm(forms.Form):
     }), required=True)
 
     def save(self, data):
-        if len(data['prototype']) > MAX_PROTOTYPE_LIMIT:
-            raise forms.ValidationError('prototype size must be less than 5MB')
         create_prototype(data)
+
+
+def validate_form(form):
+    if form.is_valid():
+        validate = URLValidator()
+        try:
+            validate(form.cleaned_data['name'])
+        except:
+            raise forms.ValidationError('please enter a valid prototype name.')
+
+        if len(form.cleaned_data['prototype']) > MAX_PROTOTYPE_LIMIT:
+            raise forms.ValidationError('prototype size must be less than 5MB')
 
 
 
@@ -52,13 +63,12 @@ def prototype_listing(request):
     form = PrototypeForm(initial={'prototype': DEFAULT_PROTOTYPE})
     if request.method == 'POST':
         form = PrototypeForm(request.POST)
-        if form.is_valid():
-            try:
-                form_data = form.cleaned_data
-                form_data['name'] = form_data['name'] + '.html'
-                form.save(form_data)
-            except Exception, ex:
-                form.errors['error'] = ' ' + ex.message
+        try:
+            validate_form(form)
+            form.save(form.cleaned_data)
+        except Exception, ex:
+            form.errors['error'] = ' ' + ex.message
+
     context = {
         'prototypes': os.listdir(settings.PROTOTYPE_DIR),
         'form': form
